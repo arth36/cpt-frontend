12ef2c3f27290b98c32064c1ecd20390be2f4de1
Arth Modi <arthmodi36@gmail.com>
2020-02-10 22:39:48 +0530

    files uploaded

---
+++ dist/precache-manifest.a138df586bc316ea67b9b7f7d14ce277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "58852aa514aad74af36b",
+    "url": "/js/chunk-vendors.1c916066.js"
+  },
+  {
+    "revision": "4db736dd962c52ec9536",
+    "url": "/js/app.2c6167fc.js"
+  },
+  {
+    "revision": "87b9a4c7b5343ff9ac929cd51455c8a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58852aa514aad74af36b",
+    "url": "/css/chunk-vendors.192de301.css"
+  },
+  {
+    "revision": "4db736dd962c52ec9536",
+    "url": "/css/app.ba9cf6a5.css"
+  }
+];
